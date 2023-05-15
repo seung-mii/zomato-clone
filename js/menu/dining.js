ef8614ce27f74544780e3942f0bd676aade1065d
withsme <tmdal9953@gmail.com>
2023-05-15 14:46:08 +0900

    17th commit : 메뉴페이지 Swiper 동작 및 End Result 스타일 추가

---
+++ js/menu/dining.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper-container', {
+  loop: true,       
+  spaceBetween: 20, 
+  slidesPerView: 4, 
+  slidesPerGroup: 1, 
+  navigation: { 
+    prevEl: '.swiper-prev', 
+    nextEl: '.swiper-next' 
+  }
+})
+
+$('.swiper-container').hover(
+  function () {
+  swiper.autoplay.stop();
+}, function(){
+  swiper.autoplay.start();
+});
