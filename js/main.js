497f41d0e708a500d94e38fd16715a8e3e07a28e
withsme <tmdal9953@gmail.com>
2024-09-03 14:49:34 +0900

    Style : Email Input 구현

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const popularSeeMore = document.querySelector("main ul.popular_menu>li.more");
 const popularHidden = document.querySelectorAll("main ul.popular_menu>li#hidden_popular");
 const type = document.getElementsByName("type_choose");
-const email = document.querySelector("main .app_menu form.app_input #email");
 const phone = document.querySelector("main .app_menu form.app_input .app_phone");
 const phonePreBtn = document.querySelector("main .app_menu form.app_input .phone_btn");
 const phonePreList = document.querySelector("main .app_menu form.app_input ul");
 const phonePreSpan = document.querySelector("main .app_menu form.app_input .phone_pre span"); 
+const appEmailInput = document.querySelector('main .app_menu form.app_input #email');
+const appEmailLabel = document.querySelector('main .app_menu form.app_input .email-label');
+const appEmailErrorMessage = document.querySelector('main .app_menu form.app_input .email_error_message');
+const appLineBtn = document.querySelector('main .app_menu form.app_input .link_btn');
 const exploreSeeMore = document.querySelectorAll("main .explore_options .explore h3");
 const exploreHidden1 = document.querySelectorAll("main .explore_options ul.cuisines>li");
 const exploreHidden2 = document.querySelectorAll("main .explore_options ul.restaurant>li");
@@ -23,12 +26,12 @@ function onPopularSeeMoreClick(event) {
 }
 
 function onEmailClick(event) {
+    appEmailInput.style.display = "inline";
     phone.style.display = "none";
-    email.style.display = "inline";
 }
 
 function onPhoneClick(event) {
-    email.style.display = "none";
+    appEmailInput.style.display = "none";
     phone.style.display = "inline";
 }
 
@@ -80,15 +83,67 @@ function onExploreSeeMoreClickThr(event) {
     } else {
         exploreHidden3.forEach((item) => {
             item.style.display = "none";
-        })
+        });
     }
 }
+ 
+function onEmailInputFocus(event) {
+    if (appEmailInput.value.trim() === '' && appEmailErrorMessage.style.display === 'block') {
+        appEmailLabel.style.color = 'red';
+        appEmailInput.style.borderColor = 'red';
+        appLineBtn.disabled = true;
+    } else {
+        appEmailErrorMessage.style.display = 'none';
+        appEmailLabel.style.color = 'green';
+        appEmailInput.style.borderColor = 'green';
+        appLineBtn.disabled = false;
+    }
+    appEmailLabel.style.transform = 'translateY(-40%)';
+    appEmailLabel.style.fontSize = '10px';
+};
+
+function onEmailInputBlur(event) {
+    if (appEmailInput.value.trim() === '') {
+        appEmailErrorMessage.style.display = 'block';
+        appEmailLabel.style.color = 'rgb(148, 148, 148)';
+        appEmailInput.style.borderColor = 'red';
+        appEmailLabel.style.transform = 'translateY(-50%)';
+        appEmailLabel.style.fontSize = '16px';
+        appLineBtn.disabled = true;
+    } else {
+        appEmailErrorMessage.style.display = 'none';
+        appEmailLabel.style.color = 'green';
+        appEmailInput.style.borderColor = '#ddd';
+        appEmailLabel.style.transform = 'translateY(-40%)';
+        appEmailLabel.style.fontSize = '10px';
+        appLineBtn.disabled = false;
+    }
+};
+
+function onEmailInput(event) {
+    if (appEmailInput.value.trim() === '') {
+        appEmailErrorMessage.style.display = 'block';
+        appEmailLabel.style.color = 'red';
+        appEmailInput.style.borderColor = 'red';
+        appLineBtn.disabled = true;
+    } else {
+        appEmailErrorMessage.style.display = 'none';
+        appEmailLabel.style.color = 'green';
+        appEmailInput.style.borderColor = 'green';
+        appLineBtn.disabled = false;
+    }
+    appEmailLabel.style.transform = 'translateY(-40%)';
+    appEmailLabel.style.fontSize = '10px';
+};
 
 popularSeeMore.addEventListener("click", onPopularSeeMoreClick);
 type[0].addEventListener("click", onEmailClick);
 type[1].addEventListener("click", onPhoneClick);
 phonePreBtn.addEventListener("click", onPhonePreClick); 
 phonePreList.addEventListener("click", onPhoneDropdownClick);
+appEmailInput.addEventListener("focus", onEmailInputFocus);
+appEmailInput.addEventListener("blur", onEmailInputBlur);
+appEmailInput.addEventListener("input", onEmailInput);
 exploreSeeMore[0].addEventListener("click", onExploreSeeMoreClickOne);
 exploreSeeMore[1].addEventListener("click", onExploreSeeMoreClickTwo);
 exploreSeeMore[2].addEventListener("click", onExploreSeeMoreClickThr);
