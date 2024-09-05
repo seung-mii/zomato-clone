ddb287ab54ff28d10113fdffefcd74489d713481
withsme <tmdal9953@gmail.com>
2024-09-05 16:05:12 +0900

    Style : Filter Modal overlay 및 btn 구현

--- js/menu/order_online.js
+++ js/menu/order_online.js
@@ -1,17 +1,17 @@
 const filterBtn = document.querySelector("main .filter_btn");
 const filterModalBackground = document.querySelector("main .filter_modal_on");
 const filterModal = document.querySelector("main .filter");
+const filterModalCloseBtn = document.querySelector("main .filter #close");
 const filterModalClearBtn = document.querySelector("main .filter .modal_footer button:first-child");
+const filterModalApplyBtn = document.querySelector("main .filter .modal_footer button:nth-child(2)"); // Fixed the selector for the Apply button
 const sortByLi = document.querySelector("main .filter .filters_type .filters_subtype li:first-child");
 const cuisinesLi = document.querySelector("main .filter .filters_type .filters_subtype li:nth-child(2)");
 const ratingLi = document.querySelector("main .filter .filters_type .filters_subtype li:nth-child(3)");
 const costPerPersonLi = document.querySelector("main .filter .filters_type .filters_subtype li:nth-child(4)");
-const moreFilterLi = document.querySelector("main .filter .filters_type .filters_subtype li:last-child");
 const filterSortBy = document.querySelector("main .filter .filters_type .sort_by");
 const filterCuisines = document.querySelector("main .filter .filters_type .cuisines");
 const filterRating = document.querySelector("main .filter .filters_type .rating");
 const filterCostPerPerson = document.querySelector("main .filter .filters_type .cost");
-const filterMoreFilter = document.querySelector("main .filter .filters_type .more_filters");
 const filterIcon = document.querySelector("main .filter .modal_header span");
 const cuisinesBtn = document.querySelector(".filter_type button:nth-child(4)");
 const cuisines = document.querySelector(".filter_type .cuisines_option");
@@ -25,139 +25,26 @@ const exploreHidden2 = document.querySelectorAll("main .explore_options ul.resta
 const exploreHidden3 = document.querySelectorAll("main .explore_options .explore_cities ul>li");
 const pageUpBtn = document.getElementById("page_up");
 
-function onFilterBtn(event) {
-  if (window.getComputedStyle(filterModal).display == "none") {
-    filterModal.style.display = "block";
-    filterModalBackground.style.width = "100vw";
-    filterModalBackground.style.height = "100vh";
-  } else {
-    filterModal.style.display = "none";
-    filterModalBackground.style.width = "0vw";
-    filterModalBackground.style.height = "0vh";
-  }
+function onFilterModal() {
+  filterModal.style.display = filterModal.style.display === 'none' ? 'block' : 'none';
+  filterModalBackground.style.display = filterModalBackground.style.display === 'none' ? 'block' : 'none';
 }
 
-function onFilterIcon(event) {
-  filterModal.style.display = "none";
-  filterModalBackground.style.width = "0vw";
-  filterModalBackground.style.height = "0vh";
-}
+function onFilterReset() {
+  sortByLi.style.borderLeft = "5px solid transparent";
+  cuisinesLi.style.borderLeft = "5px solid transparent";
+  ratingLi.style.borderLeft = "5px solid transparent";
+  costPerPersonLi.style.borderLeft = "5px solid transparent";
 
-function onFilterSortBy(event) {
-  if (window.getComputedStyle(filterSortBy).display == "none") {
-    filterSortBy.style.display = "block";
-    sortByLi.style.borderLeft = "5px solid #FF7171";
-    sortByLi.style.backgroundColor = "#fff";
-    // sortByLi.style.transition = "border-left 1s";
+  sortByLi.style.backgroundColor = "#f9f9f9";
+  cuisinesLi.style.backgroundColor = "#f9f9f9";
+  ratingLi.style.backgroundColor = "#f9f9f9";
+  costPerPersonLi.style.backgroundColor = "#f9f9f9";
 
-    cuisinesLi.style.borderLeft = "5px solid transparent";
-    ratingLi.style.borderLeft = "5px solid transparent";
-    costPerPersonLi.style.borderLeft = "5px solid transparent";
-    moreFilterLi.style.borderLeft = "5px solid transparent";
-
-    cuisinesLi.style.backgroundColor = "#f9f9f9";
-    ratingLi.style.backgroundColor = "#f9f9f9";
-    costPerPersonLi.style.backgroundColor = "#f9f9f9";
-    moreFilterLi.style.backgroundColor = "#f9f9f9";
-
-    filterCuisines.style.display = "none";
-    filterRating.style.display = "none";
-    filterCostPerPerson.style.display = "none";
-    filterMoreFilter.style.display = "none";
-  }
-}
-
-function onFilterCuisines(event) {
-  if (window.getComputedStyle(filterCuisines).display == "none") {
-    filterCuisines.style.display = "block";
-    cuisinesLi.style.borderLeft = "5px solid #FF7171";
-    cuisinesLi.style.backgroundColor = "#fff";
-    // cuisinesLi.style.transition = "border-left 1s";
-    
-    sortByLi.style.borderLeft = "5px solid transparent";
-    ratingLi.style.borderLeft = "5px solid transparent";
-    costPerPersonLi.style.borderLeft = "5px solid transparent";
-    moreFilterLi.style.borderLeft = "5px solid transparent";
-
-    sortByLi.style.backgroundColor = "#f9f9f9";
-    ratingLi.style.backgroundColor = "#f9f9f9";
-    costPerPersonLi.style.backgroundColor = "#f9f9f9";
-    moreFilterLi.style.backgroundColor = "#f9f9f9";
-
-    filterSortBy.style.display = "none";
-    filterRating.style.display = "none";
-    filterCostPerPerson.style.display = "none";
-    filterMoreFilter.style.display = "none";
-  }
-}
-
-function onFilterRating(event) {
-  if (window.getComputedStyle(filterRating).display == "none") {
-    filterRating.style.display = "block";
-    ratingLi.style.borderLeft = "5px solid #FF7171";
-    ratingLi.style.backgroundColor = "#fff";
-    
-    sortByLi.style.borderLeft = "5px solid transparent";
-    cuisinesLi.style.borderLeft = "5px solid transparent";
-    costPerPersonLi.style.borderLeft = "5px solid transparent";
-    moreFilterLi.style.borderLeft = "5px solid transparent";
-
-    sortByLi.style.backgroundColor = "#f9f9f9";
-    cuisinesLi.style.backgroundColor = "#f9f9f9";
-    costPerPersonLi.style.backgroundColor = "#f9f9f9";
-    moreFilterLi.style.backgroundColor = "#f9f9f9";
-
-    filterSortBy.style.display = "none";
-    filterCuisines.style.display = "none";
-    filterCostPerPerson.style.display = "none";
-    filterMoreFilter.style.display = "none";
-  }
-}
-
-function onFilterCostPerPerson(event) {
-  if (window.getComputedStyle(filterCostPerPerson).display == "none") {
-    filterCostPerPerson.style.display = "block";
-    costPerPersonLi.style.borderLeft = "5px solid #FF7171";
-    costPerPersonLi.style.backgroundColor = "#fff";
-    
-    sortByLi.style.borderLeft = "5px solid transparent";
-    cuisinesLi.style.borderLeft = "5px solid transparent";
-    ratingLi.style.borderLeft = "5px solid transparent";
-    moreFilterLi.style.borderLeft = "5px solid transparent";
-
-    sortByLi.style.backgroundColor = "#f9f9f9";
-    cuisinesLi.style.backgroundColor = "#f9f9f9";
-    ratingLi.style.backgroundColor = "#f9f9f9";
-    moreFilterLi.style.backgroundColor = "#f9f9f9";
-
-    filterSortBy.style.display = "none";
-    filterCuisines.style.display = "none";
-    filterRating.style.display = "none";
-    filterMoreFilter.style.display = "none";
-  }
-}
-
-function onFilterMoreFilter(event) {
-  if (window.getComputedStyle(filterMoreFilter).display == "none") {
-    filterMoreFilter.style.display = "block";
-    moreFilterLi.style.borderLeft = "5px solid #FF7171";
-    moreFilterLi.style.backgroundColor = "#fff";
-    
-    sortByLi.style.borderLeft = "5px solid transparent";
-    cuisinesLi.style.borderLeft = "5px solid transparent";
-    ratingLi.style.borderLeft = "5px solid transparent";
-    costPerPersonLi.style.borderLeft = "5px solid transparent";
-
-    sortByLi.style.backgroundColor = "#f9f9f9";
-    cuisinesLi.style.backgroundColor = "#f9f9f9";
-    ratingLi.style.backgroundColor = "#f9f9f9";
-    costPerPersonLi.style.backgroundColor = "#f9f9f9";
-
-    filterSortBy.style.display = "none";
-    filterCuisines.style.display = "none";
-    filterRating.style.display = "none";
-    filterCostPerPerson.style.display = "none";
-  }
+  filterSortBy.style.display = "none";
+  filterCuisines.style.display = "none";
+  filterRating.style.display = "none";
+  filterCostPerPerson.style.display = "none";
 }
 
 function initializeSwipers(event) {
@@ -180,6 +67,42 @@ function initializeSwipers(event) {
       nextEl: '.brands_background .swiper-next'
     }
   });
+}
+
+function onFilterSortBy() {
+  if (window.getComputedStyle(filterSortBy).display == "none") {
+    onFilterReset();
+    filterSortBy.style.display = "block";
+    sortByLi.style.borderLeft = "5px solid #FF7171";
+    sortByLi.style.backgroundColor = "#fff";
+  }
+}
+
+function onFilterCuisines() {
+  if (window.getComputedStyle(filterCuisines).display == "none") {
+    onFilterReset();
+    filterCuisines.style.display = "block";
+    cuisinesLi.style.borderLeft = "5px solid #FF7171";
+    cuisinesLi.style.backgroundColor = "#fff";
+  }
+}
+
+function onFilterRating() {
+  if (window.getComputedStyle(filterRating).display == "none") {
+    onFilterReset();
+    filterRating.style.display = "block";
+    ratingLi.style.borderLeft = "5px solid #FF7171";
+    ratingLi.style.backgroundColor = "#fff";
+  }
+}
+
+function onFilterCostPerPerson() {
+  if (window.getComputedStyle(filterCostPerPerson).display == "none") {
+    onFilterReset();
+    filterCostPerPerson.style.display = "block";
+    costPerPersonLi.style.borderLeft = "5px solid #FF7171";
+    costPerPersonLi.style.backgroundColor = "#fff";
+    }
 }
 
 function onCuisinesBtn(event) {
@@ -271,14 +194,16 @@ function onClearAllBtnClick() {
   });
 }
 
-filterBtn.addEventListener("click", onFilterBtn);
-filterModalClearBtn.addEventListener("click", onFilterBtn);
-filterIcon.addEventListener("click", onFilterIcon);
+filterBtn.addEventListener("click", onFilterModal);
+filterModalBackground.addEventListener("click", onFilterModal);
+filterModalCloseBtn.addEventListener("click", onFilterModal);
+// filterModalClearBtn.addEventListener("click", onFilterModal);
+filterModalApplyBtn.addEventListener("click", onFilterModal);
+filterIcon.addEventListener("click", onFilterModal);
 sortByLi.addEventListener("click", onFilterSortBy);
 cuisinesLi.addEventListener("click", onFilterCuisines);
 ratingLi.addEventListener("click", onFilterRating);
 costPerPersonLi.addEventListener("click", onFilterCostPerPerson);
-moreFilterLi.addEventListener("click", onFilterMoreFilter);
 cuisines.addEventListener("mouseup", onCuisinesMouseUp);
 cuisinesBtn.addEventListener("click", onCuisinesBtn);
 clearAllBtn.addEventListener("click", onClearAllBtnClick);
